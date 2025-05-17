c888f03ceb4a2164a5d7ae3bd60b60984465d446
joshyorko <joshua.yorko@gmail.com>
2025-05-17 13:36:57 -0400

    Refactor fetch-robocorp workflow and start.sh script for improved clarity and efficiency

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 rcc run -t repos -e devdata/env-for-producer.json
-rm -rf output/*
+
 rcc run -t producer -e devdata/env-for-producer.json
-rm -rf output/*
+
 rcc run -t consumer -e devdata/env-for-consumer.json
